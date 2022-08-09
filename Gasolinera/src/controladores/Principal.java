a9c700de812494aa6c82d52e705022f3c0c1d079
Macarena <Macarena@DESKTOP-5A2HH1S>
2022-08-09 10:44:04 +0200

    gasolinera con formularios

--- Gasolinera/src/controladores/Principal.java
+++ Gasolinera/src/controladores/Principal.java
@@ -4,29 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 import modelo.Combustible;
+import modelo.Repostaje;
 import vistas.FormularioAlta;
+import vistas.ListadoVentas;
 
 public class Principal {
 	
 	private FormularioAlta fAlta;
 	private List<Combustible> listaCombustibles;
+	private ListadoVentas fListado;
+	private List<Repostaje> ListaRepostajes;
 	
 
 	public Principal() {
 		fAlta=new FormularioAlta();
-		listaCombustibles=new ArrayList<Combustible>();
+		fListado=new ListadoVentas();
 		
+		ListaRepostajes=new ArrayList<Repostaje>();
+		
+		
+		listaCombustibles=new ArrayList<Combustible>();
 		listaCombustibles.add(new Combustible("Gasolina",1.794));
 		listaCombustibles.add(new Combustible("Gasoil",1.773));
 		listaCombustibles.add(new Combustible("Gasolina Plus",1.832));
 		listaCombustibles.add(new Combustible("Gasoil Plus",1.842));
 		fAlta.setListaCombustibles(listaCombustibles);
-		fAlta.setVisible(true);
 		
+		fAlta.setVisible(true);	
+	}
+	
+	public void insertarRepostaje(Repostaje r) {
+		ListaRepostajes.add(r);
+		fAlta.setVisible(false);
+		
+		fListado.setListaRepostajes(ListaRepostajes);
+		
+		
+		fListado.setVisible(true);
 	}
 
 	public static void main(String[] args) {
 		Principal ppal=new Principal();
+		
+		
+		
 
 	}
 
--- Gasolinera/src/vistas/ListadoVentas.java
+++ Gasolinera/src/vistas/ListadoVentas.java
@@ -7,10 +7,24 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+
+import modelo.Repostaje;
+
+import javax.swing.ScrollPaneConstants;
+import java.awt.Font;
 
 public class ListadoVentas extends JFrame {
-
-	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -34,14 +48,43 @@ public class ListadoVentas extends JFrame {
 	public ListadoVentas() {
 		setTitle("Listado");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 537, 385);
+		getContentPane().setLayout(new MigLayout("", "[grow][]", "[][grow][]"));
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		contentPane.add(tabbedPane, BorderLayout.NORTH);
+		JLabel lblNewLabel = new JLabel("Listado de Ventas");
+		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 14));
+		getContentPane().add(lblNewLabel, "cell 0 0");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		getContentPane().add(scrollPane, "cell 0 1,grow");
+		
+		table = new JTable();
+		table.setFont(new Font("Verdana", Font.PLAIN, 14));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Matr\u00EDcula", "Nombre", "DNI", "Combustible", "Litros", "Total", "Agrario", "Bonificacion Gobierno", "Porque Tu Vuelves"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class, Object.class, Double.class, Double.class, Boolean.class, Boolean.class, Boolean.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		scrollPane.setViewportView(table);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setFont(new Font("Verdana", Font.PLAIN, 14));
+		getContentPane().add(btnVolver, "cell 0 2,alignx center");
+	}
+
+	public void setListaRepostajes(List<Repostaje> listaRepostajes) {
+		
+		
 	}
 
 }
